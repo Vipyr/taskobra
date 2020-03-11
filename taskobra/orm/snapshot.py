6c2821cd9b5bf8784c70f8f9703b051c536dc6de
Tom Manner <tsmanner@us.ibm.com>
2020-03-11 01:21:34 -0400

    More experimentation with pruning.  Added reliable pruning start time calculation and fixed bug where the first snapshot being pruned was being partially skipped.

--- taskobra/orm/snapshot.py
+++ taskobra/orm/snapshot.py
@@ -29,22 +29,26 @@ class Snapshot(ORMBase):
 
     @classmethod
     def prune(cls, t0, snapshots: Collection["Snapshot"]):
+        t0 = min(t0, max(snapshot.timestamp for snapshot in snapshots))
+        print(f"Pruning from {t0}")
         snapshot_iterable = reversed(
             sorted(
                 filter(
-                    lambda snapshot: snapshot.timestamp <= t0, snapshots
+                    lambda snapshot: snapshot.timestamp <= t0,
+                    snapshots,
                 ),
-                key=lambda snapshot: snapshot.timestamp
-            )
+                key=lambda snapshot: snapshot.timestamp,
+            ),
         )
         prunes = []
-        snaps = [next(snapshot_iterable)]
-        sample_exponent = 1
-        sample_rate = snaps[0].sample_rate
-        sample_base = snaps[0].sample_base
-        time_slice = (snaps[0].timestamp, snaps[0].timestamp - timedelta(seconds=sample_rate * sample_base ** sample_exponent))
+        snaps = []
+        sample_exponent = 1.0
+        sample_rate = None
+        sample_base = None
+        # time_slice = (snaps[0].timestamp, snaps[0].timestamp - timedelta(seconds=sample_rate * sample_base ** sample_exponent))
+        time_slice = (t0, t0)
         for snapshot in snapshot_iterable:
-            if time_slice[0] > snapshot.timestamp >= time_slice[1]:
+            if time_slice and time_slice[0] > snapshot.timestamp >= time_slice[1]:
                 sample_rate = (sample_rate * len(snaps) + snapshot.sample_rate) / (len(snaps) + 1)
                 sample_base = (sample_base * len(snaps) + snapshot.sample_base) / (len(snaps) + 1)
                 time_slice = (time_slice[0], time_slice[0] - timedelta(seconds=sample_rate * sample_base ** sample_exponent))
@@ -54,9 +58,9 @@ class Snapshot(ORMBase):
                     [
                         [
                             metrics_by_type[type(metric)].append(metric)
-                            for metric in snapshot.metrics
+                            for metric in snap.metrics
                         ]
-                        for snapshot in snaps
+                        for snap in snaps
                     ]
                     pruned_metrics = []
                     [pruned_metrics.extend(metric_type.prune(metric))
@@ -101,7 +105,12 @@ class Snapshot(ORMBase):
         for snap in prunes:
             print(snap)
             [print(f"    {metric}") for metric in snap.metrics]
+        return prunes
+
+    @property
+    def sample_dt(self):
+        return self.sample_rate * self.sample_base ** self.sample_exponent
 
     def __repr__(self):
         total_metrics = sum(metric.sample_count for metric in self.metrics)
-        return f"<Snapshot({self.timestamp} : {self.sample_rate}*{self.sample_base}^{self.sample_exponent} : {total_metrics})>"
+        return f"<Snapshot({self.timestamp} : {self.sample_rate:.3}*{self.sample_base:.3}^{self.sample_exponent:.3}({self.sample_dt:.3}) : {total_metrics})>"
--- tests/orm/test_Snapshot.py
+++ tests/orm/test_Snapshot.py
@@ -43,37 +43,11 @@ class TestSnapshotMetric(Metric):
 
 class TestSnapshot(ORMTestCase):
     def test_prune(self):
-        t0 = datetime(2020, 3, 9, 9, 53)
         snapshots = [
-            Snapshot(
-                metrics=[
-                    TestSnapshotMetric(field=0, mean=2.0),
-                    TestSnapshotMetric(field=1, mean=4.0),
-                ],
-                sample_rate=2.0,
-                timestamp=t0
-            ),
-            Snapshot(
-                metrics=[
-                    TestSnapshotMetric(field=0, mean=3.0),
-                    TestSnapshotMetric(field=1, mean=5.0),
-                ],
-                timestamp=t0 - timedelta(seconds=2.0)
-            ),
-            Snapshot(
-                metrics=[
-                    TestSnapshotMetric(field=0, mean=2.5),
-                    TestSnapshotMetric(field=1, mean=4.5),
-                ],
-                timestamp=t0 - timedelta(seconds=5.0)
-            ),
-            Snapshot(
-                metrics=[
-                    TestSnapshotMetric(field=0, mean=2.5),
-                    TestSnapshotMetric(field=1, mean=4.5),
-                ],
-                timestamp=t0 - timedelta(seconds=3.0)
-            ),
+            Snapshot(timestamp=datetime(2020, 3, 9, 9, 53, 53), metrics=[TestSnapshotMetric(field=0, mean=2.0), TestSnapshotMetric(field=1, mean=4.0)], sample_rate=2.0),
+            Snapshot(timestamp=datetime(2020, 3, 9, 9, 53, 48), metrics=[TestSnapshotMetric(field=0, mean=3.0), TestSnapshotMetric(field=1, mean=5.0)]),
+            Snapshot(timestamp=datetime(2020, 3, 9, 9, 53, 50), metrics=[TestSnapshotMetric(field=0, mean=2.5), TestSnapshotMetric(field=1, mean=4.5)]),
+            Snapshot(timestamp=datetime(2020, 3, 9, 9, 53, 47), metrics=[TestSnapshotMetric(field=0, mean=2.5), TestSnapshotMetric(field=1, mean=4.5)]),
         ]
 
         print()
@@ -81,65 +55,8 @@ class TestSnapshot(ORMTestCase):
             print(snapshot)
             [print(f"    {metric}") for metric in snapshot.metrics]
 
-        print("Pruned:")
-        snapshot = Snapshot.prune(t0, snapshots)
-        # print("Pruned:")
-        # snapshot = Snapshot.prune(t0 - timedelta(seconds=1), snapshots)
-        # print(snapshot)
-        # [print(f"    {metric}") for metric in snapshot.metrics]
-
-        class SnapshotMock:
-            def __init__(self, t, rate, base):
-                self.t = t
-                self.rate = rate
-                self.base = base
-
-            def __repr__(self):
-                return f"({self.t}, {self.rate}, {self.base})"
-
-        ts = [
-            # SnapshotMock(0.0, 1.0, 2.0),
-            # SnapshotMock(1.0, 1.0, 2.0),
-            SnapshotMock(3.0, 1.0, 2.0),
-            SnapshotMock(4.0, 1.0, 3.0),
-            SnapshotMock(5.0, 1.0, 2.0),
-            SnapshotMock(6.0, 1.0, 2.0),
-            SnapshotMock(7.0, 1.0, 2.0),
-            SnapshotMock(8.0, 1.0, 2.0),
-            SnapshotMock(9.0, 1.0, 2.0),
-            SnapshotMock(10.0, 1.0, 2.0),
-            SnapshotMock(11.0, 1.0, 2.0),
-            SnapshotMock(12.0, 1.0, 2.0),
-            SnapshotMock(13.0, 1.0, 2.0),
-            SnapshotMock(14.0, 1.0, 2.0),
-            SnapshotMock(2.0, 1.0, 2.0),
-            SnapshotMock(15.0, 1.0, 2.0),
-            SnapshotMock(16.0, 1.0, 2.0),
-        ]
-
-        prunes = []
-        time_slice = None
-        exp = 1
-        rate = None
-        base = None
-        snaps = []
-        for sm in sorted(ts, key=lambda snap: snap.t):
-            if  rate is not None and base is not None and time_slice[0] <= sm.t < time_slice[1]:
-                rate = (rate * len(snaps) + sm.rate) / (len(snaps) + 1)
-                base = (base * len(snaps) + sm.base) / (len(snaps) + 1)
-                time_slice = (time_slice[0], time_slice[0] + rate * base ** exp)
-            elif snaps:
-                prunes.append((time_slice, snaps))
-                snaps = []
-                exp += 1
-                rate = sm.rate
-                base = sm.base
-                time_slice = (time_slice[1], time_slice[1] + rate * base ** exp)
-            else:
-                rate = sm.rate
-                base = sm.base
-                time_slice = (sm.t, sm.t + rate * base ** exp)
-            snaps.append(sm)
-        prunes.append((time_slice, snaps))
-
-        # [print(snaps) for snaps in prunes]
+        pruned_snapshots = Snapshot.prune(datetime(2020, 3, 9, 9, 53, 53), snapshots)
+        pruned_snapshots.append(
+            Snapshot(timestamp=datetime(2020, 3, 9, 9, 53, 55), metrics=[TestSnapshotMetric(field=0, mean=3.0), TestSnapshotMetric(field=1, mean=5.0)]),
+        )
+        Snapshot.prune(datetime(2020, 3, 9, 9, 55, 57), pruned_snapshots)
