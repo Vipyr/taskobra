203810f2a400ec6e0a0c349afda38e8724baeec0
Miguel Nistal <nistam328@gmail.com>
2020-04-03 18:26:23 -0400

    Added an api route

--- taskobra/web/views/api.py
+++ taskobra/web/views/api.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify
+import json
 
+from taskobra.orm import *
 
 blueprint = Blueprint('api', __name__, url_prefix='/api')
 
@@ -10,4 +12,13 @@ def base():
     Probably TODO remove, and replace with interesting routes 
     """
     return jsonify({})
+    
+@blueprint.route('/hostnames')
+def hostnames():
+    """
+    Base API Route, not sure what this should return
+    Probably TODO remove, and replace with interesting routes 
+    """
+    hostnames = [system.name for system in System.query.all()]
+    return json.dumps(hostnames)
     
--- taskobra/web/views/ui.py
+++ taskobra/web/views/ui.py
@@ -1,7 +1,5 @@
 from flask import Blueprint, render_template
 
-from taskobra.orm import *
-
 blueprint = Blueprint('ui', __name__)
 
 @blueprint.route('/')
@@ -9,5 +7,4 @@ def index():
     """
     Base application view, landing page 
     """
-    systems = System.query.all()
     return render_template('home.html')
