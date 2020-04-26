9f870299c9a8c58e05791ad79d67ab600731472d
Miguel Nistal <nistam328@gmail.com>
2020-04-26 15:39:51 -0400

    Updated with discussion

--- taskobra/monitor/__main__.py
+++ taskobra/monitor/__main__.py
@@ -1,40 +1,29 @@
-
-import asyncio 
 import argparse 
 import psutil
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", "--verbose", help="Increase output verbosity", action="store_true")
     return parser.parse_args()
 
-async def cpu_monitoring(args):
+def main(args):
     while True:
-        await asyncio.sleep(5)
-        print(f"CPU     : {psutil.cpu_percent()}")
-
-async def memory_monitoring(args):
-    while True:
-        await asyncio.sleep(5)
+        # Create SnapShot()
+        # Call Each function, have each return a metric 
+        print(f"Disk    : {psutil.disk_usage('/')}")
         print(f"VMem    : {psutil.virtual_memory()}")
         print(f"SwapMem : {psutil.swap_memory()}")
-
-async def disk_monitoring(args):
-    while True:
-        await asyncio.sleep(5)
-        print(f"Disk    : {psutil.disk_usage('/')}")
+        print(f"CPU     : {psutil.cpu_percent()}")
+        # snapshot = for each metric snapshot.add(metric) 
+        # with db connection of some sort 
+            # db of some sort.commit(snapshot)
 
 
 if __name__ == "__main__":
     args = parse_args()
-    loop = asyncio.get_event_loop()
     try:
-        # TODO: If we deprecate pre-3.7 we can make this create_task
-        task = asyncio.ensure_future(cpu_monitoring(args)) 
-        task = asyncio.ensure_future(memory_monitoring(args)) 
-        task = asyncio.ensure_future(disk_monitoring(args)) 
-        loop.run_forever()
+        main(args)
     except KeyboardInterrupt:
         pass
-    finally:
-        loop.close()
+    # TODO: Add handlers for SIGINT, SIGKILL, SIGABORT, SIGTERM
