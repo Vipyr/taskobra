198ba5c3e17e090ac039bb30672ee5d391c77b79
Tom Manner <tsmanner@us.ibm.com>
2020-04-28 12:16:45 -0400

    Refactored the packaging a bit to put the abstract dependencies into setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@ from setuptools import setup
 setup(
     name = "taskobra",
     packages = [
+        "taskobra.monitor",
         "taskobra.orm",
+        "taskobra.web",
     ],
     install_requires = [
+        "flask",
+        "psutil",
+        "psycopg2",
         "sqlalchemy",
     ],
 )
