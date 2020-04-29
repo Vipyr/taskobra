cda2e0cca0f3dc118b9c79ce06bbfecac235da43
Miguel Nistal <nistam328@gmail.com>
2020-04-29 13:52:54 -0400

    Added unit tests for the new api routes

--- tests/web/test_routes.py
+++ tests/web/test_routes.py
@@ -2,13 +2,7 @@ from .WebTestCase import WebTestCase
 import flask
 
 class TestRoutes(WebTestCase):
-
     def test_home_route_context(self):
-        """ NOTE: I'm not totally sure what the difference between request_context and the client object are
-                  I think the difference is that the test_client executes the request, 
-                  while the request_context drops us into the server-side code as if we're making the request
-                  Could be useful, leaving this in for reference/future use
-        """
         with self.app.test_request_context('/'):
             assert flask.request.path == '/'
 
@@ -27,5 +21,29 @@ class TestRoutes(WebTestCase):
     def test_api_systems_route(self):
         with self.app.test_client() as client:
             rsp = client.get('/api/systems')
+            assert rsp.content_type == 'application/json'
+            assert rsp.status_code == 200
+
+    def test_api_metrics_cpu_route(self):
+        with self.app.test_client() as client:
+            rsp = client.get('/api/metrics/cpu')
+            assert rsp.content_type == 'application/json'
+            assert rsp.status_code == 200
+
+    def test_api_metrics_gpu_route(self):
+        with self.app.test_client() as client:
+            rsp = client.get('/api/metrics/gpu')
+            assert rsp.content_type == 'application/json'
+            assert rsp.status_code == 200
+
+    def test_api_metrics_memory_route(self):
+        with self.app.test_client() as client:
+            rsp = client.get('/api/metrics/memory')
+            assert rsp.content_type == 'application/json'
+            assert rsp.status_code == 200
+
+    def test_api_metrics_storage_route(self):
+        with self.app.test_client() as client:
+            rsp = client.get('/api/metrics/storage')
             assert rsp.content_type == 'application/json'
             assert rsp.status_code == 200
